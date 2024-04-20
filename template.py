3d5a1f27ae7d9c2bd376b529f0c765ef69dfc600
akesh-0909 <akeshkumar65885@gmail.com>
2024-04-20 12:25:29 +0530

    testing env setup

--- template.py
+++ template.py
@@ -3,31 +3,30 @@ import os
 # os.path.join we use this to join paths acc to system because diff systems, have diff-2 syles of paths
 
 
-dirs  = [
-    os.path.join('data','raw'),
-    os.path.join('data','proecssed'),
-   'notebooks',
+dirs = [
+    os.path.join('data', 'raw'),
+    os.path.join('data', 'proecssed'),
+    'notebooks',
     'saved_models',
     "src"
-    
+
 ]
 
 
 for dir_ in dirs:
-    os.makedirs(dir_,exist_ok=True) # if true it will create if dir not exits else do nothing (if present)
-    with open(os.path.join(dir_,".gitkeep"),'w') as f:
+    # if true it will create if dir not exits else do nothing (if present)
+    os.makedirs(dir_, exist_ok=True)
+    with open(os.path.join(dir_, ".gitkeep"), 'w') as f:
         pass
 
-files =[
+files = [
     "dvc.yaml",
     "params.yaml",
-    ".gitignore",  # files are not pushed which are mentioned in this 
-    os.path.join("src","__init__.py")
+    ".gitignore",  # files are not pushed which are mentioned in this
+    os.path.join("src", "__init__.py")
     # README.md etc fioles
 ]
 
 for file_ in files:
-    with open(file_,'w'):
+    with open(file_, 'w'):
         pass
-    
-    
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,18 @@
+import pytest
 
-def test_genric():
-    a =2
-    b=2 
-    assert a == b
+
+class NotInRange(Exception):
+    def __init__(self, message="Value not in Range"):
+        self.message = message
+        super().__init__(self.message)
+
+
+def test_generic():
+    a = 5
+    with pytest.raises(NotInRange):
+        if a not in range(10, 20):
+            raise NotInRange
+
+
+def test_something():
+    assert True
