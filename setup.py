02b65423ce646ae684b1f664c5ae623452e463bf
akesh-0909 <akeshkumar65885@gmail.com>
2024-04-20 09:54:04 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="src",
+    version="0.0.1",
+    description=  """Its Wine quality package""",
+    author = 'akesh',
+    packages= find_packages(),
+    license="MIT"
+    
+    
+)
+
